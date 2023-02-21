3e2f48aba0ea333bdcc544fb1dea147de3500f1f
kcp <2842961263@qq.com>
2023-02-22 01:19:38 +0800

    更换插件

---
+++ 剑指Offer/剑指 Offer 09.用两个栈实现队列.js
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=剑指 Offer 09 lang=javascript
+ * @lcpr version=21704
+ *
+ * [剑指 Offer 09] 用两个栈实现队列
+ */
+
+// @lc code=start
+
+var CQueue = function() {
+
+};
+
+/** 
+ * @param {number} value
+ * @return {void}
+ */
+CQueue.prototype.appendTail = function(value) {
+
+};
+
+/**
+ * @return {number}
+ */
+CQueue.prototype.deleteHead = function() {
+
+};
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * var obj = new CQueue()
+ * obj.appendTail(value)
+ * var param_2 = obj.deleteHead()
+ */
+// @lc code=end
+
+// @lcpr-div-debug-arg-start
+// funName=
+// paramTypes= []
+// returnType=
+// @lcpr-div-debug-arg-end
+
+
+/*
+// @lcpr case=start
+// 
+// @lcpr case=end
+
+// @lcpr case=start
+// 
+// @lcpr case=end
+
+ */
+
+
